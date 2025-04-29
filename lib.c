10f1b14d5cf5e76ff4ffdc3cd679d750ae69d0f5
sheng153 <nicolas.sabio@gmail.com>
2025-04-28 21:11:43 -0300

    Presentacion simple

---
+++ lib.c
@@ -0,0 +1,12 @@
+#include "./lib.h"
+
+const char* c_get_weather_data(const char* city) {}
+
+const char* c_get_forecast(const char* city) {}
+
+float c_get_temperature(const char* city) {}
+
+float c_get_humidity(const char* city) {}
+
+float c_get_wind_speed(const char* city) {}
+
