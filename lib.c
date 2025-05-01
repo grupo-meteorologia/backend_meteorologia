ce105d6f6c63ab67777fe1361b9e3443470055d6
Adriano Rovelli <arovelli.dev@gmail.com>
2025-05-01 11:42:36 -0300

    Comentarios sobres las funciones principales de la aplicacion

--- lib.c
+++ lib.c
@@ -1,12 +1,17 @@
 #include "./lib.h"
 
+// Datos del clima
 const char* c_get_weather_data(const char* city) {}
 
+//Meteorologo semanal
 const char* c_get_forecast(const char* city) {}
 
+//Obtener Temperatura
 float c_get_temperature(const char* city) {}
 
+//Obtener humedad
 float c_get_humidity(const char* city) {}
 
+//Obtener viento
 float c_get_wind_speed(const char* city) {}
 
